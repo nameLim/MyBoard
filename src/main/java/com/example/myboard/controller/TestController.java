9f37ac6d433ca99bba60ade5bf5a276af6ebe6a2
nameLim <lim49435150@gmail.com>
2021-11-21 14:55:58 +0900

    배포 연습

--- src/main/java/com/example/myboard/controller/TestController.java
+++ src/main/java/com/example/myboard/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("test")
     public String test() {
-        String testStr = " Hi~ ";
+        String testStr = " Hi~ git 에서 on 변경";
         System.out.println(testStr);
         return testStr;
     }
