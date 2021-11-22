0f0900d0a594e228bde41c7a4451a4812c2f9a9a
nameLim <lim49435150@gmail.com>
2021-11-23 00:51:10 +0900

    RDS 체크 배포

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ fi
 
 echo "> $JAR_PATH 배포"
 #nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
-nohup java -jar $JAR_PATH &
+nohup java -jar $JAR_PATH --spring.config.location=$REPOSITORY/build/libs/application.properties &
 
 #nohup java -jar -Dspring.config.location=classpath:/application.properties
