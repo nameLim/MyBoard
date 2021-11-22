16cf24804036c418cb5c4b60c9f15d125f7ea33a
nameLim <lim49435150@gmail.com>
2021-11-23 01:01:09 +0900

    RDS 체크 배포322

--- src/main/java/com/example/myboard/controller/TestController.java
+++ src/main/java/com/example/myboard/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test() {
-        String testStr = "RDS 체크 배포3";
+        String testStr = "RDS 체크 배포322";
         System.out.println(testStr);
         return testStr;
     }
