3039f0ec660da728d916a741e2c214c242cd26f9
nameLim <lim49435150@gmail.com>
2021-11-23 02:00:56 +0900

    yaml, shell 위치 변경

--- src/main/java/com/example/myboard/controller/TestController.java
+++ src/main/java/com/example/myboard/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test() {
-        String testStr = "RDS 체크 배포322";
+        String testStr = "yaml, shell 위치 변경";
         System.out.println(testStr);
         return testStr;
     }
