ba4801ccf85e0402871eec529614351003ea8ee1
nameLim <lim49435150@gmail.com>
2021-11-22 16:47:28 +0900

    deploy shell 파일 변경

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ REPOSITORY=/opt/testapp
 cd $REPOSITORY
 
 APP_NAME=action_codedeploy
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
