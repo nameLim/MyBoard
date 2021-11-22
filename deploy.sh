85ac0df3d2ddb61b31ab778825ee66ced37d906c
nameLim <lim49435150@gmail.com>
2021-11-22 17:35:41 +0900

    shell APP_NAME변경

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/opt/testapp
 cd $REPOSITORY
 
-APP_NAME=action_codedeploy
+APP_NAME=MyBoard
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
