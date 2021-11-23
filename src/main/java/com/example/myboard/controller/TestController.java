0a645850b263f0b3939caea083da14abbbfae7ab
nameLim <lim49435150@gmail.com>
2021-11-23 19:13:46 +0900

    cofing 추가->테스트를 위해 옮김

--- src/main/java/com/example/myboard/controller/TestController.java
+++ src/main/java/com/example/myboard/controller/TestController.java
@@ -27,7 +27,7 @@ public class TestController {
     @PostMapping("/test2")
     public String test2(@ModelAttribute FrontRequestCardDto frontRequestCardDto) throws IOException {
         mypageService.modifyFrontCard(frontRequestCardDto);
-        String testStr = "test2완료33333333333333333333";
+        String testStr = "tㄹㄹ33";
         System.out.println(testStr);
         return testStr;
     }
