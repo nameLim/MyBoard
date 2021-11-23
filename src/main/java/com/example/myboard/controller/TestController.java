63f33c8b043306887898858542f14a209798c05c
nameLim <lim49435150@gmail.com>
2021-11-23 16:01:38 +0900

    다지움

--- src/main/java/com/example/myboard/controller/TestController.java
+++ src/main/java/com/example/myboard/controller/TestController.java
@@ -2,7 +2,6 @@ package com.example.myboard.controller;
 
 import com.example.myboard.dto.FrontRequestCardDto;
 import com.example.myboard.dto.FrontResponseCardDto;
-import com.example.myboard.service.MypageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,20 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @RestController
-@RequiredArgsConstructor
 public class TestController {
 
-    private MypageService mypageService;
 
     @GetMapping("/test")
     public String test() {
-        String testStr = "application.properies 2변경3";
+        String testStr = "ㄹㄹㄹ";
         System.out.println(testStr);
         return testStr;
     }
 
-    @PostMapping("/test2")
-    public FrontResponseCardDto test2(@ModelAttribute FrontRequestCardDto frontRequestCardDto) throws IOException {
-        return mypageService.modifyFrontCard(frontRequestCardDto);
-    }
 }
